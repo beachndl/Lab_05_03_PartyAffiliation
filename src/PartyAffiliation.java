df0b36e775df68cb1196c7fd49f494d0dca5c6d6
Daniel Beachnau <beachndl@mail.uc.edu>
2025-02-10 23:07:23 -0500

    Changed name of Main to PartyAffiliation

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class PartyAffiliation {
     public static void main(String[] args) {
         // Scanner + variable declaration
         Scanner in = new Scanner(System.in);
