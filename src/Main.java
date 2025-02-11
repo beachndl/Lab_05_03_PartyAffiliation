b5b41dabe7a01c14953db4ed611573b3c1930828
Daniel Beachnau <beachndl@mail.uc.edu>
2025-02-10 22:39:56 -0500

    Completed Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Scanner + variable declaration
+        Scanner in = new Scanner(System.in);
+        String menuChoice = "";
+        String menu = "D = Democrat\tR = Republican\tI = Independent\nChoose your party: ";
+
+        // Display menu and get user input
+        System.out.print(menu);
+        menuChoice = in.nextLine();
+
+        // Process and output
+        if (menuChoice.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        }
+        else if (menuChoice.equalsIgnoreCase("R")) {
+            System.out.println("You get a Republican Elephant.");
+        }
+        else if (menuChoice.equalsIgnoreCase("I")) {
+            System.out.println("You get an Independent Man.");
+        }
+        else {
+            System.out.println("Invalid. You must choose D, R, or I, not: " + menuChoice);
+        }
     }
 }
